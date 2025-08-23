9ba7c6ab08d429f6e177d698914090cf292733d6
kevin <auwaikevin@gmail.com>
2025-08-23 13:18:40 +0800

    merge components 8

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -6,7 +6,7 @@ import { Container } from 'react-bootstrap';
 const Home = () => {
   return (
     <>
-   <iframe width="100%" height="600" src="https://www.youtube.com/embed/Gnx4XUvEd44?si=G-L1erEWVtzgRp72&amp;start=10" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+   <iframe width="100%" height="600" src="https://www.youtube.com/embed/Gnx4XUvEd44?si=G-L1erEWVtzgRp72&amp;start=10&autoplay=1&loop=1" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
    <Container className='home_container'>
       {/* <Section title="facilities"/> */}
       <Section title="news"/>
